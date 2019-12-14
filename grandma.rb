a7c74725fba5c90a875aa77005eca24085b59877
Damely Tineo <damely.tineo07@gmail.com>
2019-12-14 02:10:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma (message)
   message = "MAMA"
   if message == message.upcase 
     puts "NO, NOT SINCE 1938!"
-  else 
+  elsif message == message.lowcase
     puts "HUH?! SPEAK UP, SONNY!"
 end
 
